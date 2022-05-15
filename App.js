9cb2abfc7e417076f0530469da8ff40b4f172d9c
Nishat Ahmed <nishatahmed4485@gmail.com>
2022-05-15 09:43:59 +0600

    first home

---
+++ App.js
@@ -0,0 +1,28 @@
+import * as React from 'react'
+import { View, StyleSheet } from 'react-native'
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
+import HomeScreen from './HomeScreen';
+
+const Stack = createStackNavigator();
+
+export default class App extends React.Component{
+  render(){
+    return(
+      <View style= {styles.container}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      backgroundColor: '#FFF'
+  }
+})
