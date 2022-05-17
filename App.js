5ca3f5fd13498639f20bab353377c28f55003fe4
Nishat Ahmed <nishatahmed4485@gmail.com>
2022-05-17 16:09:55 +0600

    Login

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { View, StyleSheet } from 'react-native'
 import {NavigationContainer} from '@react-navigation/native'
 import {createStackNavigator} from '@react-navigation/stack'
 import HomeScreen from './HomeScreen';
+import LoginScreen from ',/LoginScreen';
 
 const Stack = createStackNavigator();
 
@@ -13,6 +14,8 @@ export default class App extends React.Component{
         <NavigationContainer>
           <Stack.Navigator>
             <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Login"component={LoginScreen} />
+            
           </Stack.Navigator>
         </NavigationContainer>
       </View>
